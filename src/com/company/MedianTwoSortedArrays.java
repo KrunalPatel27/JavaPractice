3c567a49553b7a6e4e6c37e01e02e77b1c6e6160
Krunal Patel <KRUNAL.PATEL@VIZIENTINC.COM>
2019-03-21 19:47:03 -0500

    median done

--- src/com/company/MedianTwoSortedArrays.java
+++ src/com/company/MedianTwoSortedArrays.java
@@ -31,33 +31,34 @@ public class MedianTwoSortedArrays {
     b = {1,3,5,7,9}
 
      */
-    int[] a = {5,6,7,8,9}; //count = 5
-    int[] b = {1,3,5,7,9}; //count = 5
+    int[] a = {1,3,5,7,9}; //count = 5
+    int[] b = {1,2,3,4,5}; //count = 5
     int partition1, partition2;
     int lengthA = a.length, lengthB = b.length, totalLength = lengthA + lengthB;
     int start=0, end =0;
     //a+b = {1,3,5,5,6,7,7,8,9,9}
     public MedianTwoSortedArrays(){
-        start =0;
-        end = a.length/2;
-        partition1 = end;
+        partition1 = lengthA/2 ;
         partition2 = (totalLength +1)/2 - partition1;
         findMedian();
-        System.out.println(a[partition1] +" p1:p2 " +b[partition2]);
+        System.out.println(Math.max(a[partition1-1], b[partition2-1])
+                + "left:right"
+                + Math.min(a[partition1], b[partition2]) );
     }
 
     private void findMedian() {
-        if(a[partition1-1] > b[partition2-1]){
+        System.out.println(partition1 +" p1::p2 " +partition2);
+        if(a[partition1-1] > b[partition2]){
             int diff = partition1 - (partition1/2);
             partition1 = partition1/2;
             partition2 +=diff;
             findMedian();
-        }if(b[partition2-1-1]> a[partition1]){
+        }if(b[partition2-1]> a[partition1]){
             int diff = partition2 - (partition2/2);
             partition2 = partition2/2;
             partition1 +=diff;
             findMedian();
         }
-        System.out.println(partition1 +" p1:p2 " +partition2);
+        System.out.println(partition1 +"  p1:p2 " +partition2);
     }
 }
