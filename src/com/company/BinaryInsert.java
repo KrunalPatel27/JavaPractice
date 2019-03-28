ff1196a1ed449007b7fc3a46d73e974298d98396
KrunalPatel27 <kpate236@gmail.com>
2019-03-27 22:18:55 -0500

    fix binary insert

--- src/com/company/BinaryInsert.java
+++ src/com/company/BinaryInsert.java
@@ -3,14 +3,14 @@ package com.company;
 import java.util.Arrays;
 
 public class BinaryInsert {
-    int[]arr1 = {1,2,3,5,6,4};
+    int[]arr1 = {5,16,27,45,55,65,9};
     public BinaryInsert(){
         for (int i: arr1) {
-            System.out.println(i);
+            System.out.println("initial: "+i);
         }
         insert(arr1.length-1);
         for (int i: arr1) {
-            System.out.println(i);
+            System.out.println("final: "+i);
         }
     }
     //insert end in a sorted array in place
@@ -18,37 +18,35 @@ public class BinaryInsert {
         int val = arr1[end];
         int gap = end/2;
         int pick = end/2;
-        while(pick >=0 || pick < end){
+        while(pick >=0 || pick <= end){
             gap /=2;
-            if(pick ==0 || pick == end-1){
-
+            if(gap ==0)gap=1;
+            if(pick ==0 || pick == end){
+                insertAt(pick,end);
+                return;
             }
             if(arr1[pick] > val){  // go left
                 if(arr1[pick-1]<val){
-                    insertAt(pick-1, end);
+                    insertAt(pick, end);
                     return;
                 }
                 pick -= gap;
             }
             else{  // go right
                 if(arr1[pick+1] > val){
-                    insertAt(pick+1, end);
+                    insertAt(pick, end);
                     return;
                 }
                 pick+= gap;
             }
-            val = arr1[pick];
-
         }
         insertAt(pick, end);
     }
 
     private void insertAt(int i, int end) {
-        System.out.println("call");
         int swap = arr1[end];
         int temp;
         while(i<=end){
-//            System.out.println("print "+i);
             temp = arr1[i];
             arr1[i] = swap;
             swap = temp;
