f5167b93d714de0e2ad47f3897d95264ab5c08e0
Krunal Patel <krunalpatel@Krunals-MBP.T-mobile.com>
2019-04-08 20:30:31 -0500

    longest palindrome substring

---
+++ src/com/company/DynamicProgramming/LongestPalindrome.java
@@ -0,0 +1,37 @@
+package com.company.DynamicProgramming;
+
+public class LongestPalindrome {
+    int[][] grid;
+    int max =1;
+    int maxX =0, maxY = 0;
+
+    public LongestPalindrome(){
+        System.out.println(longestPalindrome("bananas"));
+    }
+
+    public String longestPalindrome(String s) {
+        int length= s.length();
+        grid = new int[length][length];
+        for(int i =0; i< length; i++){
+            grid[i][i] = 1;
+        }
+        for(int i=1; i<length; i++){
+            int x=i,y=0;
+            for(int j=0; j<length-i; j++){
+                if(s.charAt(j) == s.charAt(i+j) ){
+                    grid[y][x] = grid[y+1][x-1] + 1;
+                    if(max<grid[y][x]){
+                        max = grid[y][x];
+                        maxY = y;
+                        maxX = x;
+                    }
+                }else{
+                    grid[y][x] = 0;
+                }
+                y++;
+                x++;
+            }
+        }
+        return s.substring(maxY, maxX+1);
+    }
+}
---
+++ src/com/company/DynamicProgramming/MainDynamicProgramming.java
@@ -0,0 +1,13 @@
+package com.company.DynamicProgramming;
+
+public class MainDynamicProgramming {
+
+    public static void main (String[] args){
+        switch (Integer.parseInt(args[0])){
+            case 1:
+                LongestPalindrome longestPalindrome = new LongestPalindrome();
+                break;
+        }
+    }
+
+}
