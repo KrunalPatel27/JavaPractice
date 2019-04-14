94dce5d57998b43cae4d546609c77f33d0785c38
Krunal Patel <kpate236@gmail.com>
2019-04-13 22:35:27 -0500

    add test

--- src/test/com/Trees/SumOfAllNodesTest.java
+++ src/test/com/Trees/SumOfAllNodesTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SumOfAllNodesTest {
 
     @org.junit.jupiter.api.Test
+    //add tests
     void sum() {
         SumOfAllNodes sumOfAllNodes = new SumOfAllNodes();
         int val = sumOfAllNodes.sum(sumOfAllNodes.root);
